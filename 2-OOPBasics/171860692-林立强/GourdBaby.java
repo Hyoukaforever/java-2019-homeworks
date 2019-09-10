d4fd5d1fcedfc3e42e7e34b0ec126fe3e2f22e14
Hyoukaforever <36329946+Hyoukaforever@users.noreply.github.com>
2019-09-10 11:59:07 +0800

    Update GourdBaby.java

--- 2-OOPBasics/171860692-林立强/GourdBaby.java
+++ 2-OOPBasics/171860692-林立强/GourdBaby.java
@@ -5,7 +5,8 @@ class GourdBaby {
     void report(int pos1,int pos2){
         System.out.printf("%s: %d -> %d\n",name,pos1,pos2);
     }
-    void say(){System.out.print(name);}
+    void sayname(){System.out.print(name);}
+    void saycolor(){System.out.print(color);}
     GourdBaby(int idx,String name,String color){
         this.idx=idx;
         this.name=name;
